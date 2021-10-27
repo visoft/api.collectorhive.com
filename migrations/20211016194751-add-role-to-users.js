c78b3cfb70b3b2c8a93269b8d2c26f19467a73d9
Damien White <damien.white@visoftinc.com>
2021-10-27 18:51:04 -0400

    Fixed migration

--- migrations/20211016194751-add-role-to-users.js
+++ migrations/20211016194751-add-role-to-users.js
@@ -1,15 +1,13 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Users', 'role',         {
-        type: Sequelize.STRING,
-        allowNull: false,
-        defaultValue: 'user'
-      },)
+    await queryInterface.addColumn('Users', 'role', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'user',
+    });
   },
 
   down: async (queryInterface) => {
-    await queryInterface.removeColumn(
-      Users, 'role'
-    )
-  }
+    await queryInterface.removeColumn('Users', 'role');
+  },
 };
